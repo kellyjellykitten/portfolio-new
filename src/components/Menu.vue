006ea4753bb50ecd255fdb1fc667426d1eebd5df
kelly <kellybackwards@gmail.com>
2022-11-29 16:24:46 -0500

    bulma navbar

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,24 +1,44 @@
 <script setup>
-import { ref } from 'vue';
-
-const items = ref([
-  {
-    label:'Home',
-    icon:'pi pi-fw pi-home',
-    to:'/'
-  },
-  {
-    label:'About',
-    icon:'pi pi-fw pi-info-circle',
-    to:'/about'
-  }
-])
 </script>
 
 <template>
-  <div>
-    <Menu :model="items" />
-  </div>
+  <nav class="navbar" role="navigation" aria-label="main navigation">
+    <div class="navbar-brand">
+      <a class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28" />
+      </a>
+
+      <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+        <span aria-hidden="true"></span>
+      </a>
+    </div>
+
+    <div id="navbarBasicExample" class="navbar-menu">
+      <div class="navbar-start">
+        <a class="navbar-item">
+          <router-link to="/" class="nav-link">Home</router-link>
+        </a>
+        <a class="navbar-item">
+          <router-link to="/about" class="nav-link">About</router-link>
+        </a>
+      </div>
+
+      <div class="navbar-end">
+        <div class="navbar-item">
+          <div class="buttons">
+            <a class="button is-primary">
+              <strong>Button1</strong>
+            </a>
+            <a class="button is-light">
+              Button2
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <style scoped>
--- src/main.js
+++ src/main.js
@@ -2,19 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import PrimeVue from 'primevue/config'
-import Menu from 'primevue/menu';
-
-import './assets/app.css'
-import 'primevue/resources/themes/arya-green/theme.css' //theme
-import 'primevue/resources/primevue.min.css' //core css
-import 'primeicons/primeicons.css' //icons
+// import './assets/app.css'
 
 const app = createApp(App)
 
 app.use(router)
-app.use(PrimeVue)
-
-app.component('Menu', Menu)
 
 app.mount('#app')
