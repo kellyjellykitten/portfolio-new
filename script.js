58a014e005a899bdb6f2e6c8517a36c22402efaf
kelly <kellybackwards@gmail.com>
2023-08-23 15:33:12 -0400

    adding bckgrnd animation

--- script.js
+++ script.js
@@ -35,18 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+        strings: ["front-end web developer."],
         typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
     });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
+    // var typed = new Typed(".typing-2", {
+    //     strings: ["front-end web developer."],
+    //     typeSpeed: 100,
+    // });
 
     // owl carousel script
     $('.carousel').owlCarousel({
