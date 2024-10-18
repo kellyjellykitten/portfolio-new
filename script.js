14464cd4ecd034661d3e10a3732995fc2f7d850d
kelly <kellybackwards@gmail.com>
2024-10-18 17:22:51 -0400

    contact form submit js, about me content update, added last proj descriptions

--- script.js
+++ script.js
@@ -55,3 +55,46 @@ window.onclick = function(event) {
 }
 
 //Stop scroll animation from activating upon scrolling back up page
+
+//Contact form submit function
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+
+function sendEmail() {
+    const bodyMessage = `Name: ${fullName.value}<br> Email: ${email.value}<br> Message: ${message.value}`
+
+    Email.send({
+        SecureToken: "09e22744-f49f-4543-a99c-f5e8ee956415",
+        Username: "kellybackwards@gmail.com",
+        To: 'kellybackwards@gmail.com',
+        From: "kellybackwards@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then(
+        message => {
+            if (message == "OK") {
+                Swal.fire({
+                    title: "Success!",
+                    text: "Message sent!",
+                    icon: "success"
+                });
+            }
+            else{
+                Swal.fire({
+                    title: "Error!",
+                    text: "Message could not be sent",
+                    icon: "error"
+                })
+            }
+        }
+    );
+}
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    sendEmail();
+})
