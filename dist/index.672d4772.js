2f774484eb74b9be19a20bf7ccc8112f9e6f136a
kelly <kellybackwards@gmail.com>
2024-10-18 21:29:29 -0400

    build using parcel2 & first deploy

---
+++ dist/index.672d4772.js
@@ -0,0 +1,79 @@
+$(document).ready(function() {
+    $(window).scroll(function() {
+        // sticky navbar on scroll script
+        if (this.scrollY > 20) $(".navbar").addClass("sticky");
+        else $(".navbar").removeClass("sticky");
+        // scroll-up button show/hide script
+        if (this.scrollY > 500) $(".scroll-up-btn").addClass("show");
+        else $(".scroll-up-btn").removeClass("show");
+    });
+    // slide-up script
+    $(".scroll-up-btn").click(function() {
+        $("html").animate({
+            scrollTop: 0
+        });
+        // removing smooth scroll on slide-up button click
+        $("html").css("scrollBehavior", "auto");
+    });
+    $(".navbar .menu li a").click(function() {
+        // applying again smooth scroll on menu items click
+        $("html").css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $(".menu-btn").click(function() {
+        $(".navbar .menu").toggleClass("active");
+        $(".menu-btn i").toggleClass("active");
+    });
+});
+//AWS skills dropdown
+//When user clicks button, toggle b/w hiding & showing the dropdown content
+const openSkills = ()=>{
+    console.log("clicked toggle");
+    document.getElementById("skillsDropdown").classList.toggle("show");
+};
+//Close dropdown if user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches(".dropbtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for(i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) openDropdown.classList.remove("show");
+        }
+    }
+};
+//Stop scroll animation from activating upon scrolling back up page
+//Contact form submit function
+const form = document.querySelector("form");
+const fullName = document.getElementById("name");
+const email = document.getElementById("email");
+const subject = document.getElementById("subject");
+const message = document.getElementById("message");
+function sendEmail() {
+    const bodyMessage = `Name: ${fullName.value}<br> Email: ${email.value}<br> Message: ${message.value}`;
+    Email.send({
+        SecureToken: "09e22744-f49f-4543-a99c-f5e8ee956415",
+        Username: "kellybackwards@gmail.com",
+        To: "kellybackwards@gmail.com",
+        From: "kellybackwards@gmail.com",
+        Subject: subject.value,
+        Body: bodyMessage
+    }).then((message)=>{
+        if (message == "OK") Swal.fire({
+            title: "Success!",
+            text: "Message sent!",
+            icon: "success"
+        });
+        else Swal.fire({
+            title: "Error!",
+            text: "Message could not be sent",
+            icon: "error"
+        });
+    });
+}
+form.addEventListener("submit", (e)=>{
+    e.preventDefault();
+    sendEmail();
+});
+
+//# sourceMappingURL=index.672d4772.js.map
