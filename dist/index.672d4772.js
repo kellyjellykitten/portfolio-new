4d81a89ad7248c0da64ebde378f826bb1e820fec
kelly <kellybackwards@gmail.com>
2024-10-19 22:42:49 -0400

    overflow fix; contact section mobile styling

--- dist/index.672d4772.js
+++ dist/index.672d4772.js
@@ -1,37 +1,51 @@
 $(document).ready(function() {
     $(window).scroll(function() {
-        // sticky navbar on scroll script
+        //Sticky navbar on scroll script
         if (this.scrollY > 20) $(".navbar").addClass("sticky");
         else $(".navbar").removeClass("sticky");
-        // scroll-up button show/hide script
+        //Scroll-up button show/hide script
         if (this.scrollY > 500) $(".scroll-up-btn").addClass("show");
         else $(".scroll-up-btn").removeClass("show");
     });
-    // slide-up script
+    //Slide-up script
     $(".scroll-up-btn").click(function() {
         $("html").animate({
             scrollTop: 0
         });
-        // removing smooth scroll on slide-up button click
+        //Removing smooth scroll on slide-up button click
         $("html").css("scrollBehavior", "auto");
     });
     $(".navbar .menu li a").click(function() {
-        // applying again smooth scroll on menu items click
+        //Applying again smooth scroll on menu items click
         $("html").css("scrollBehavior", "smooth");
     });
-    // toggle menu/navbar script
+    //Toggle menu/navbar script
     $(".menu-btn").click(function() {
         $(".navbar .menu").toggleClass("active");
         $(".menu-btn i").toggleClass("active");
     });
 });
-//AWS skills dropdown
-//When user clicks button, toggle b/w hiding & showing the dropdown content
+//Toggle hamburger menu with X
+const menuBtn = document.querySelector(".menu-btn");
+//Set initial state of icon
+let showMenu = false;
+menuBtn.addEventListener("click", toggleIcon);
+function toggleIcon() {
+    if (!showmenu) {
+        menuBtn.classList.add("close");
+        showMenu = true;
+    } else {
+        menuBtn.classList.remove("close");
+        showMenu = false;
+    }
+}
+//AWS skills accordian
+//When user clicks button, toggle b/w hiding & showing the content
 const openSkills = ()=>{
     console.log("clicked toggle");
     document.getElementById("skillsDropdown").classList.toggle("show");
 };
-//Close dropdown if user clicks outside of it
+//Close accordian if user clicks outside of it
 window.onclick = function(event) {
     if (!event.target.matches(".dropbtn")) {
         var dropdowns = document.getElementsByClassName("dropdown-content");
@@ -42,7 +56,6 @@ window.onclick = function(event) {
         }
     }
 };
-//Stop scroll animation from activating upon scrolling back up page
 //Contact form submit function
 const form = document.querySelector("form");
 const fullName = document.getElementById("name");
